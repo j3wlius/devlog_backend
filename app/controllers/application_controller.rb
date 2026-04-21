95e3873d0e0b2b7836a9f486bc2918735191423f
j3wlius <tamalejulius5@gmail.com>
2026-04-21 22:17:10 +0300

    feat: created auth and entries controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secret_key_base)
+  end
+
+  def decode_token(token)
+    JWT.decode(token, Rails.application.secret_key_base)[0]
+  rescue JWT::DecodeError
+      nil
+  end
+
+  def current_user
+    header = request.headers["Authorization"]
+    token = header&.split(" ")&.last
+    decoded = decode_token(token)
+    @current_user ||= User.find(decoded["user_id"]) if decoded
+  end
+
+  def authenticate_user!
+    render json: { error: "Unauthorized" }, status: :unauthorized unless current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # auth routes
-  post "signup", to: "auth#register"
-  post "login", to: "auth#login"
-  # delete "logout", to: "auth#logout"
+  namespace :api do
+    namespace :v1 do
+      # auth routes
+      post "signup", to: "auth#register"
+      post "login", to: "auth#login"
+      # delete "logout", to: "auth#logout"
+
+      resources :entries
+    end
+  end
 end
